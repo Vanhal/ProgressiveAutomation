191fbe1040dd9d47a394670edcc97483455875c5
Vanhal <obriensh@gmail.com>
2014-05-15 21:58:23 +0100

    autoupdate 2014-05-15-21:58:23

--- src/main/java/com/vanhal/progressiveautomation/gui/GuiHandler.java
+++ src/main/java/com/vanhal/progressiveautomation/gui/GuiHandler.java
@@ -5,9 +5,13 @@ import java.lang.reflect.Constructor;
 import gnu.trove.map.TMap;
 import gnu.trove.map.hash.THashMap;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.InventoryPlayer;
+import com.vanhal.progressiveautomation.gui.container.BaseContainer;
 
 import com.vanhal.progressiveautomation.gui.client.BaseGUI;
 
@@ -23,17 +27,25 @@ public class GuiHandler implements IGuiHandler {
 	}
 
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		TileEntity tile = world.getTileEntity(x, y, z);
-		
-		return null;
+		try {
+			TileEntity tile = world.getTileEntity(x, y, z);
+			Class<? extends BaseContainer> containerClass = (Class<? extends BaseContainer>) containers.get(ID);
+			Constructor cons = containerClass.getDeclaredConstructor(new Class[] {InventoryPlayer.class, TileEntity.class});
+			return cons.newInstance(player.inventory, tile);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		TileEntity tile = world.getTileEntity(x, y, z);
-		Class<? extends BaseGUI> guiClass = (Class<? extends BaseGUI>) guis.get(ID);
-		Constructor cons = guiClass.getDeclaredConstructor(parameterTypes);
-		
-		return null;
+		try {
+			TileEntity tile = world.getTileEntity(x, y, z);
+			Class<? extends BaseGUI> guiClass = (Class<? extends BaseGUI>) guis.get(ID);
+			Constructor cons = guiClass.getDeclaredConstructor(new Class[] {InventoryPlayer.class, TileEntity.class});
+			return cons.newInstance(player.inventory, tile);
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 	
--- src/main/java/com/vanhal/progressiveautomation/gui/container/ContainerMiner.java
+++ src/main/java/com/vanhal/progressiveautomation/gui/container/ContainerMiner.java
@@ -5,28 +5,30 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.tileentity.TileEntity;
 
 import com.vanhal.progressiveautomation.entities.BaseTileEntity;
+import com.vanhal.progressiveautomation.entities.TileMiner;
 
 public class ContainerMiner extends BaseContainer {
 
 	public ContainerMiner(InventoryPlayer inv, TileEntity entity) {
 		super((BaseTileEntity)entity);
+		TileMiner miner = (TileMiner) entity;
 		
 		//add slots
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 0, 11, 16)); //cobble
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 1, 11, 52)); //burnable
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 2, 49, 52)); //pickaxe
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 3, 74, 52)); //shovel
+		this.addSlotToContainer(new Slot(miner, 0, 11, 16)); //cobble
+		/*this.addSlotToContainer(new Slot(miner, 1, 11, 52)); //burnable
+		this.addSlotToContainer(new Slot(miner, 2, 49, 52)); //pickaxe
+		this.addSlotToContainer(new Slot(miner, 3, 74, 52)); //shovel
 		
 		//output slots
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 4, 112, 16));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 5, 130, 16));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 6, 148, 16));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 7, 112, 34));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 8, 130, 34));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 9, 148, 34));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 10, 112, 52));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 11, 130, 52));
-		this.addSlotToContainer(new Slot((BaseTileEntity)entity, 12, 148, 52));
+		this.addSlotToContainer(new Slot(miner, 4, 112, 16));
+		this.addSlotToContainer(new Slot(miner, 5, 130, 16));
+		this.addSlotToContainer(new Slot(miner, 6, 148, 16));
+		this.addSlotToContainer(new Slot(miner, 7, 112, 34));
+		this.addSlotToContainer(new Slot(miner, 8, 130, 34));
+		this.addSlotToContainer(new Slot(miner, 9, 148, 34));
+		this.addSlotToContainer(new Slot(miner, 10, 112, 52));
+		this.addSlotToContainer(new Slot(miner, 11, 130, 52));
+		this.addSlotToContainer(new Slot(miner, 12, 148, 52));*/
 		
 		addPlayerInventory(inv);
 	}
