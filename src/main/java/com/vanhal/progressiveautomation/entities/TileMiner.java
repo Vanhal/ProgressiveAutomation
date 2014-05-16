7888dc45f30c0bc6b747a684604053a64a5af724
Vanhal <obriensh@gmail.com>
2014-05-16 22:29:26 +0100

    autoupdate 2014-05-16-22:29:27

--- src/main/java/com/vanhal/progressiveautomation/entities/TileMiner.java
+++ src/main/java/com/vanhal/progressiveautomation/entities/TileMiner.java
@@ -1,10 +1,32 @@
 package com.vanhal.progressiveautomation.entities;
 
+import net.minecraft.block.Block;
+
 public class TileMiner extends BaseTileEntity {
+	protected int totalMineBlocks = -1;
+	protected int currentMineBlocks = 0;
 
 	public TileMiner() {
 		super(14);
+		if (totalMineBlocks==-1) {
+			scanBlocks();
+		}
+	}
+	
+	public void scanBlocks() {
+		boolean bedrock = false;
+		int newY = this.yCoord - 1;
+		while (!bedrock) {
+			Block tryBlock = worldObj.getBlock(this.xCoord, newY, this.zCoord);
+		}
 	}
 
+	public int getRange() {
+		if (this.getStackInSlot(4)==null) {
+			return 1;
+		} else {
+			return this.getStackInSlot(4).stackSize + 1;
+		}
+	}
 
 }
