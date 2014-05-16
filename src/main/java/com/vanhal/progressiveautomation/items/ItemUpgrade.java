3008b6b0003044fd21336bc3759a9952f8e76916
Vanhal <obriensh@gmail.com>
2014-05-16 20:44:26 +0100

    autoupdate 2014-05-16-20:44:26

--- src/main/java/com/vanhal/progressiveautomation/items/ItemUpgrade.java
+++ src/main/java/com/vanhal/progressiveautomation/items/ItemUpgrade.java
@@ -3,5 +3,10 @@ package com.vanhal.progressiveautomation.items;
 import net.minecraft.item.Item;
 
 public class ItemUpgrade extends Item {
-
+	public String itemName;
+	public int level;
+	
+	public ItemUpgrade() {
+		
+	}
 }
