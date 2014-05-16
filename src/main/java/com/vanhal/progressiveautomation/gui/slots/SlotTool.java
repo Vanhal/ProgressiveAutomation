f9278fafcf22234c8fcd206fc49661f63dac3393
Vanhal <obriensh@gmail.com>
2014-05-16 19:44:26 +0100

    autoupdate 2014-05-16-19:44:27

--- src/main/java/com/vanhal/progressiveautomation/gui/slots/SlotTool.java
+++ src/main/java/com/vanhal/progressiveautomation/gui/slots/SlotTool.java
@@ -5,11 +5,13 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 public class SlotTool extends Slot {
-	
+	protected int level;
+	protected int type;
 	
 	public SlotTool(int ToolType, int ToolLevel, IInventory par1iInventory, int par2, int par3, int par4) {
 		super(par1iInventory, par2, par3, par4);
-		
+		level = ToolLevel;
+		type = ToolType;
 	}
 
 	public boolean isItemValid(ItemStack itemStack) {
--- src/main/java/com/vanhal/progressiveautomation/ref/ToolInfo.java
+++ src/main/java/com/vanhal/progressiveautomation/ref/ToolInfo.java
@@ -1,5 +1,19 @@
 package com.vanhal.progressiveautomation.ref;
 
-public class ToolInfo {
+import net.minecraft.item.ItemStack;
 
+public class ToolInfo {
+	//tools
+	public static int TYPE_PICKAXE = 0;
+	public static int TYPE_SHOVEL = 1;
+	public static int TYPE_AXE = 2;
+	public static int TYPE_SWORD = 3;
+	public static int TYPE_HOE = 4;
+	
+	//levels
+	public static int LEVEL_WOOD = 0;
+	public static int LEVEL_STONE = 1;
+	public static int LEVEL_IRON = 2;
+	public static int LEVEL_GOLD = 3;
+	public static int LEVEL_DIAMOND = 4;
 }
