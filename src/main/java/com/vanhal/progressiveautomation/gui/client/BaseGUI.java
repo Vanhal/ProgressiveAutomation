3710119fc40617b7c0f97f2e6db949f655c7bade
Vanhal <obriensh@gmail.com>
2014-05-16 21:59:26 +0100

    autoupdate 2014-05-16-21:59:27

--- src/main/java/com/vanhal/progressiveautomation/gui/client/BaseGUI.java
+++ src/main/java/com/vanhal/progressiveautomation/gui/client/BaseGUI.java
@@ -9,7 +9,13 @@ import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
 
 public class BaseGUI extends GuiContainer {
-	public static int WHITE = 0x000000;
+	public static int BLACK = 0x000000;
+	public static int GRAY = 0x404040;
+	public static int WHITE = 0xFFFFFF;
+	public static int GREEN = 0x5CA028;
+	public static int RED = 0xCC3333;
+	public static int BLUE = 0x4C8BFF;
+	
 	
 	public static final FontRenderer fontRenderer = FMLClientHandler.instance().getClient().fontRenderer;
 	
@@ -21,18 +27,28 @@ public class BaseGUI extends GuiContainer {
 	}
 	
 	protected void drawGuiContainerForegroundLayer(int x, int y) {
-		
+		drawText();
+		drawString("Inventory", 8, 74, GRAY);
 	}
 	
+	protected void drawText() {	}
+
 	protected void drawGuiContainerBackgroundLayer(float f, int x, int y) {
 		GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 		mc.renderEngine.bindTexture(background);
 		drawTexturedModalRect(guiLeft, guiTop, 0, 0, 176, 166);
 		
 	}
+	
+	protected void drawString(String text, int y, int colour) {
+		drawString(text, ((xSize - fontRendererObj.getStringWidth(text)) / 2) , y, colour);
+	}
 
 	
 	protected void drawString(String text, int x, int y, int colour) {
-		
+		fontRendererObj.drawString(text, x, y, colour);
 	}
+	
+	
+
 }
--- src/main/java/com/vanhal/progressiveautomation/gui/client/GUIMiner.java
+++ src/main/java/com/vanhal/progressiveautomation/gui/client/GUIMiner.java
@@ -1,5 +1,6 @@
 package com.vanhal.progressiveautomation.gui.client;
 
+import com.vanhal.progressiveautomation.entities.TileMiner;
 import com.vanhal.progressiveautomation.gui.container.ContainerMiner;
 import com.vanhal.progressiveautomation.ref.Ref;
 
@@ -10,9 +11,19 @@ import net.minecraft.util.ResourceLocation;
 
 public class GUIMiner extends BaseGUI {
 	public static final ResourceLocation texture = new ResourceLocation(Ref.MODID, "textures/gui/Miner.png");
+	
+	private TileMiner miner;
 
 	public GUIMiner(InventoryPlayer inv, TileEntity entity) {
 		super(new ContainerMiner(inv, entity), texture);
+		miner = (TileMiner) entity;
+	}
+	
+	protected void drawText() {
+		drawString("Miner", 6, GRAY);
+		drawString("Status:", 38, 17, BLUE);
+		drawString("<-Cobble", 38, 27, BLUE);
+		drawString("Ready to Mine", 38, 37, BLUE);
 	}
 	
 }
