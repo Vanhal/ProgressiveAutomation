f705497fab895e4924f55b232f500aa768444505
Vanhal <obriensh@gmail.com>
2014-09-15 11:27:27 +0100

    Fixed a null pointer error when disabling a machine

--- src/main/java/com/vanhal/progressiveautomation/blocks/PABlocks.java
+++ src/main/java/com/vanhal/progressiveautomation/blocks/PABlocks.java
@@ -16,51 +16,51 @@ public class PABlocks {
 				if (PAConfig.generatorEnabled && PAConfig.rfSupport) generator[i] = new BlockGenerator(i);
 			}
 		}
-		
+
 		//preInit
 		for (int i = 0; i <= 3; i++) {
 			if (PAConfig.allowLevel(i)) {
-				miner[i].preInit();
-				chopper[i].preInit();
-				planter[i].preInit();
-				generator[i].preInit();
+				if (PAConfig.minerEnabled) miner[i].preInit();
+				if (PAConfig.chopperEnabled)chopper[i].preInit();
+				if (PAConfig.planterEnabled) planter[i].preInit();
+				if (PAConfig.generatorEnabled && PAConfig.rfSupport) generator[i].preInit();
 			}
 		}
 	}
-	
+
 	public static void init() {
 		for (int i = 0; i <= 3; i++) {
 			if (PAConfig.allowLevel(i)) {
-				miner[i].init();
-				chopper[i].init();
-				planter[i].init();
-				generator[i].init();
+				if (PAConfig.minerEnabled) miner[i].init();
+				if (PAConfig.chopperEnabled)chopper[i].init();
+				if (PAConfig.planterEnabled) planter[i].init();
+				if (PAConfig.generatorEnabled && PAConfig.rfSupport) generator[i].init();
 			}
 		}
 	}
-	
+
 	public static void postInit() {
 		for (int i = 0; i <= 3; i++) {
 			if (PAConfig.allowLevel(i)) {
-				miner[i].postInit();
-				chopper[i].postInit();
-				planter[i].postInit();
-				generator[i].postInit();
+				if (PAConfig.minerEnabled) miner[i].postInit();
+				if (PAConfig.chopperEnabled)chopper[i].postInit();
+				if (PAConfig.planterEnabled) planter[i].postInit();
+				if (PAConfig.generatorEnabled && PAConfig.rfSupport) generator[i].postInit();
 			}
 		}
 	}
-	
+
 	//blocks
-	
+
 	//miners
 	public static BlockMiner[] miner = new BlockMiner[4];
-	
+
 	//choppers
 	public static BlockChopper[] chopper = new BlockChopper[4];
-	
+
 	//planters
 	public static BlockPlanter[] planter = new BlockPlanter[4];
-	
+
 	//generators
 	public static BlockGenerator[] generator = new BlockGenerator[4];
 
--- src/main/java/com/vanhal/progressiveautomation/items/PAItems.java
+++ src/main/java/com/vanhal/progressiveautomation/items/PAItems.java
@@ -10,51 +10,52 @@ public class PAItems {
 		if (PAConfig.allowStoneLevel) stoneUpgrade = new ItemStoneUpgrade();
 		if (PAConfig.allowIronLevel) ironUpgrade = new ItemIronUpgrade();
 		if (PAConfig.allowDiamondLevel) diamondUpgrade = new ItemDiamondUpgrade();
-		if (PAConfig.allowCobbleUpgrade) cobbleUpgrade = new ItemCobbleGenUpgrade();
+		if ((PAConfig.allowCobbleUpgrade) && (PAConfig.minerEnabled)) cobbleUpgrade = new ItemCobbleGenUpgrade();
 		if (PAConfig.allowWitherUpgrade) witherUpgrade = new ItemWitherUpgrade();
-		
+
+
 		if (PAConfig.allowCoalPellets) coalPellet = new ItemCoalPellet();
-		
+
 		if (PAConfig.rfSupport) {
 			rfEngine = new ItemRFEngine();
 			CheatRFEngine = new ItemCreativeRFEngine();
 		}
-		
+
 		//preInit them
 		if (woodUpgrade!=null) woodUpgrade.preInit();
 		if (stoneUpgrade!=null) stoneUpgrade.preInit();
 		if (ironUpgrade!=null) ironUpgrade.preInit();
 		if (diamondUpgrade!=null) diamondUpgrade.preInit();
 		if (witherUpgrade!=null) witherUpgrade.preInit();
-		
+
 		if (PAConfig.rfSupport) {
 			rfEngine.preInit();
 			CheatRFEngine.preInit();
 		}
-		
+
 		if (coalPellet!=null) coalPellet.preInit();
 	}
-	
+
 	public static void init() {
 		if (cobbleUpgrade!=null) cobbleUpgrade.preInit();
 	}
-	
+
 	public static void postInit() {
-		
+
 	}
-	
+
 	//items
 	public static ItemWoodUpgrade woodUpgrade = null;
 	public static ItemStoneUpgrade stoneUpgrade = null;
 	public static ItemIronUpgrade ironUpgrade = null;
 	public static ItemDiamondUpgrade diamondUpgrade = null;
-	
+
 	public static ItemCobbleGenUpgrade cobbleUpgrade = null;
 	public static ItemWitherUpgrade witherUpgrade = null;
-	
+
 	public static ItemRFEngine rfEngine = null;
 	public static ItemRFEngine CheatRFEngine = null;
-	
+
 	public static ItemCoalPellet coalPellet = null;
-	
+
 }
