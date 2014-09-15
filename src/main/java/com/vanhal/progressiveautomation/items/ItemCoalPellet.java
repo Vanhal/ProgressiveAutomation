a6c57961b2b9ae8aa75ab8ac87db5d59c76cc5ec
Vanhal <obriensh@gmail.com>
2014-09-15 12:16:44 +0100

    Fixed a derp where I made everything into fuel....

--- src/main/java/com/vanhal/progressiveautomation/items/ItemCoalPellet.java
+++ src/main/java/com/vanhal/progressiveautomation/items/ItemCoalPellet.java
@@ -15,28 +15,32 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class ItemCoalPellet extends BaseItem implements IFuelHandler  {
 	protected int burnTime = 100;
-	
+
 	public ItemCoalPellet() {
 		super("CoalPellet");
 		setTextureName(Ref.MODID+":Coal_Pellet");
 		int coalTime = TileEntityFurnace.getItemBurnTime(new ItemStack(Items.coal));
-		burnTime = coalTime / 9;
+		burnTime = coalTime / 8;
 	}
-	
+
     protected void addNormalRecipe() {
-    	ShapelessOreRecipe recipe = new ShapelessOreRecipe(new ItemStack(this, 9), Items.coal);
+    	ShapelessOreRecipe recipe = new ShapelessOreRecipe(new ItemStack(this, 8), Items.coal);
 		GameRegistry.addRecipe(recipe);
-		recipe = new ShapelessOreRecipe(new ItemStack(this, 9), new ItemStack(Items.coal, 1, 1));
+		recipe = new ShapelessOreRecipe(new ItemStack(this, 8), new ItemStack(Items.coal, 1, 1));
 		GameRegistry.addRecipe(recipe);
-		
+
 		GameRegistry.registerFuelHandler(this);
 	}
-	
+
 	protected void addUpgradeRecipe() {
 		addNormalRecipe();
 	}
 
 	public int getBurnTime(ItemStack fuel) {
-		return burnTime;
+		if (fuel.isItemEqual(new ItemStack(this))) {
+			return burnTime;
+		} else {
+			return 0;
+		}
 	}
 }
