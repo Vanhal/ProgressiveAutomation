4448391b44ed64318c1b217b9ef83853dcd556db
ToadsworthLP-Laptop <fiery.toadsworth@gmail.com>
2017-01-28 19:18:47 +0100

    Implement capacitors to store lots of RF power.

---
+++ src/main/java/com/vanhal/progressiveautomation/entities/capacitor/TileCapacitorDiamond.java
@@ -0,0 +1,10 @@
+package com.vanhal.progressiveautomation.entities.capacitor;
+
+public class TileCapacitorDiamond extends TileCapacitor {
+	
+	public TileCapacitorDiamond() {
+		super();
+		setEnergyStorage(320000,640);
+	}
+
+}
